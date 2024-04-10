e6cf663dd87a0393f18de09419558fd231126d4f
emerson <emerson8096@gmail.com>
2024-04-10 06:37:31 -0300

    Header Component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,15 +1,14 @@
 import { Container } from './styles'
 
+import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 
 export function Details(){
 
   return(
     <Container>
-      <h1>Hello World!</h1>
+      <Header />
 
-      <Button title="Login" loading />
-      <Button title="Cadastrar"/>
       <Button title="Voltar"/>
     </Container>
   )
