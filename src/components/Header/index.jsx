13be1fc8c1772a1fe52507a2cb5eab588be47389
emerson <emerson8096@gmail.com>
2024-04-10 06:45:12 -0300

    user img

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,20 @@
-import { Container } from './style'
+import { Container, Profile  } from './style'
 
 export function Header(){
   return(
     <Container>
-    
+      <Profile >
+        <img
+          src="https://github.com/smoothemerson.png"
+          alt='Foto do usuário'
+        />
+
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Emerson Rocha</strong>
+        </div>
+
+      </Profile>
     </Container>
   )
 }
