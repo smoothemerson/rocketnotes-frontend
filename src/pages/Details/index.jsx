2c5bf7669e104c62841ef13c8ca16f0e5a76a7a8
emerson <emerson8096@gmail.com>
2024-04-11 14:57:15 -0300

    finalize details page

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from './styles'
+import { Container, Links, Content } from './styles'
 
 import { Tag } from '../../components/Tag'
 import { Button } from '../../components/Button'
@@ -12,21 +12,39 @@ export function Details(){
     <Container>
       <Header />
 
-      <ButtonText title="Excluir nota"/>
+      <main>
+        <Content>
 
-      <Section title="Links úteis">
-        <Links>
-          <li><a href="#">https://www.rocketseat.com.br</a></li>
-          <li><a href="#">https://www.rocketseat.com.br</a></li>
-        </Links>
-      </Section>
+          <ButtonText title="Excluir nota"/>
 
-      <Section title="Marcadores">
-        <Tag title="express" />
-        <Tag title="nodejs" />
-      </Section>
+          <h1>
+            Introdução ao React
+          </h1>
 
-      <Button title="Voltar"/>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Error ullam dicta ex quod eligendi quam nobis a vitae quidem, 
+            facere incidunt ipsam sunt rem minus optio tenetur unde cum adipisci.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus 
+            fuga modi facere nam. Autem, minima officia expedita quae tenetur 
+            sequi quaerat quidem sint? Deleniti magni, ipsum ex ab et dolores.
+          </p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li><a href="#">https://www.rocketseat.com.br</a></li>
+              <li><a href="#">https://www.rocketseat.com.br</a></li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="nodejs" />
+          </Section>
+
+          <Button title="Voltar"/>
+        </Content>
+      </main>
     </Container>
   )
 }
