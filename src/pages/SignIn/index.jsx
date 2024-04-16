183bdeff4c2b4cdf0a7b91c40f8749dd0cadb990
emerson <emerson8096@gmail.com>
2024-04-16 17:31:05 -0300

    using img

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -3,7 +3,7 @@ import { FiMail, FiLock } from 'react-icons/fi'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 
-import { Container, Form } from './styles'
+import { Container, Form, Background } from './styles'
 
 export function SignIn(){
   return(
@@ -35,6 +35,8 @@ export function SignIn(){
         </a>
 
       </Form>
+
+      <Background />
     </Container>
   )
 }
