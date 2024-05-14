4940257bada0af898e78ac0e941560874ede92b9
emerson <emerson8096@gmail.com>
2024-05-14 18:15:04 -0300

    auth hook

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from './styles/global'
 
-import { MyContext } from "./myContext";
+import { AuthProvider } from "./hooks/auth";
 
 import { Routes } from "./routes";
 
@@ -13,9 +13,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <MyContext.Provider value={{ NAME: 'Emerson', email: 'emerson@email.com'}}>
-        <Routes />
-      </MyContext.Provider>
+        <AuthProvider>
+          <Routes />
+        </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
