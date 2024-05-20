6d7c7e2a391b6c476de1434cff85e1f68355fdf7
emerson <emerson8096@gmail.com>
2024-05-20 17:09:55 -0300

    add signIn

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,19 +1,39 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
-export const AuthContext = createContext({})
+import { api } from "../services/api";
 
-function AuthProvider({ children }){
+export const AuthContext = createContext({});
+
+function AuthProvider({ children }) {
+  const [data, setData] = useState({})
+
+  async function signIn({ email, password }) {
+    try {
+      const response = await api.post("/sessions", { email, password });
+      const { user, token } = response.data;
+
+      api.defaults.headers.authorization = `Bearer ${token}`;
+      setData({ user, token })
+
+
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("Não foi possível entrar.");
+      }
+    }
+  }
+
   return (
-    <AuthContext.Provider value={{ NAME: 'Emerson', email: 'emerson@email.com'}}>
-      {children}
-    </AuthContext.Provider>
-  )
+    <AuthContext.Provider value={{ signIn, user: data.user }}>{children}</AuthContext.Provider>
+  );
 }
 
-function useAuth(){
-  const context = useContext(AuthContext)
+function useAuth() {
+  const context = useContext(AuthContext);
 
-  return context
+  return context;
 }
 
-export { AuthProvider, useAuth }
+export { AuthProvider, useAuth };
