2d9014944ba36a340af097142656383af2de5989
emerson <emerson8096@gmail.com>
2024-05-22 07:52:49 -0300

    using Local Storage

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,21 +1,22 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 import { api } from "../services/api";
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
-  const [data, setData] = useState({})
+  const [data, setData] = useState({});
 
   async function signIn({ email, password }) {
     try {
       const response = await api.post("/sessions", { email, password });
       const { user, token } = response.data;
 
+      localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
+      localStorage.setItem("@rocketnotes:token", token);
+
       api.defaults.headers.authorization = `Bearer ${token}`;
-      setData({ user, token })
-
-
+      setData({ user, token });
     } catch (error) {
       if (error.response) {
         alert(error.response.data.message);
@@ -25,8 +26,24 @@ function AuthProvider({ children }) {
     }
   }
 
+  useEffect(() => {
+    const token = localStorage.getItem("@rocketnotes:token");
+    const user = localStorage.getItem("@rocketnotes:user");
+
+    if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
+      setData({
+        token,
+        user: JSON.parse(user),
+      });
+    }
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ signIn, user: data.user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ signIn, user: data.user }}>
+      {children}
+    </AuthContext.Provider>
   );
 }
 
