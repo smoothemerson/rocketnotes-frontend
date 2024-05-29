b8e5fe9bc2e23f15b5eb629111eaadd746672c61
emerson <emerson8096@gmail.com>
2024-05-29 07:25:49 -0300

    search notes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -83,7 +83,7 @@ export function Home() {
         <Input
           placeholder="Pesquisar pelo título"
           icon={FiSearch}
-          onChange={() => setSearch(e.target.value)}
+          onChange={(e) => setSearch(e.target.value)}
         />
       </Search>
 
