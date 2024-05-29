2c578239c8a4b40bba50396578b545cc05757c92
emerson <emerson8096@gmail.com>
2024-05-29 07:30:27 -0300

    Open registered note

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { FiPlus, FiSearch } from "react-icons/fi";
 
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
@@ -16,6 +17,8 @@ export function Home() {
   const [tagsSelected, setTagsSelected] = useState([]);
   const [notes, setNotes] = useState([]);
 
+  const navigate = useNavigate();
+
   function handleTagSelected(tagName) {
     if (tagName === "all") {
       return setTagsSelected([]);
@@ -29,6 +32,10 @@ export function Home() {
     } else {
       setTagsSelected((prevState) => [...prevState, tagName]);
     }
+  }
+
+  function handleDetails(id) {
+    navigate(`/details/${id}`);
   }
 
   useEffect(() => {
@@ -89,8 +96,12 @@ export function Home() {
 
       <Content>
         <Section title="Minhas notas">
-          {notes.map((notes) => (
-            <Note key={String(notes.id)} data={notes} />
+          {notes.map((note) => (
+            <Note
+              key={String(note.id)}
+              data={note}
+              onClick={() => handleDetails(note.id)}
+            />
           ))}
         </Section>
       </Content>
