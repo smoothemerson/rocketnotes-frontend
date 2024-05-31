b9e46e38ee7497d1bb58c48c0f6385f8de76adb0
emerson <emerson8096@gmail.com>
2024-05-31 13:48:21 -0300

    Handling Unavailable Routes

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import { New } from '../pages/New'
 import { Home } from '../pages/Home'
@@ -14,6 +14,7 @@ export function AppRoutes(){
       <Route path='/profile' element={ <Profile />} />
       <Route path='/details/:id' element={ <Details />} />
 
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   )
 }
