be2fdf08fddbf32a3583e78621e258504fbf4cfb
emerson <emerson8096@gmail.com>
2024-05-31 13:37:05 -0300

    redirecting logout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,12 +2,19 @@ import { RiShutDownLine } from "react-icons/ri";
 import { useAuth } from "../../hooks/auth";
 
 import { api } from "../../services/api";
-import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
+import avatarPlaceholder from "../../assets/avatar_placeholder.svg";
 
 import { Container, Profile, Logout } from "./style";
+import { useNavigate } from "react-router-dom";
 
 export function Header() {
   const { signOut, user } = useAuth();
+  const navigation = useNavigate();
+
+  function handleSignOut() {
+    navigation("/");
+    signOut();
+  }
 
   const avatarURL = user.avatar
     ? `${api.defaults.baseURL}/files/${user.avatar}`
@@ -24,7 +31,7 @@ export function Header() {
         </div>
       </Profile>
 
-      <Logout onClick={signOut}>
+      <Logout onClick={handleSignOut}>
         <RiShutDownLine />
       </Logout>
     </Container>
