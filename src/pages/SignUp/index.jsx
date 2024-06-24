31ac5295c5fa4b8e9ab2870d192473ba84ddb920
emerson <emerson8096@gmail.com>
2024-06-24 08:48:54 -0300

    refactoring and add enter functionality

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,83 +1,90 @@
-import { useState } from 'react'
-import { FiMail, FiLock, FiUser } from 'react-icons/fi'
-import { Link, useNavigate } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { FiMail, FiLock, FiUser } from "react-icons/fi";
+import { Link, useNavigate } from "react-router-dom";
 
-import { api } from "../../services/api"
+import { api } from "../../services/api";
 
-import { Input } from '../../components/Input'
-import { Button } from '../../components/Button'
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
 
-import { Container, Form, Background } from './styles'
+import { Container, Form, Background } from "./styles";
 
-export function SignUp(){
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  function handleSignUp(){
-    if(!name || !email || !password){
-      return alert("Preencha todos os campos!")
+  function handleSignUp() {
+    if (!name || !email || !password) {
+      return alert("Preencha todos os campos!");
     }
 
-    api.post("/users", { name, email, password })
+    api
+      .post("/users", { name, email, password })
       .then(() => {
-        alert("Usuário cadastrado com sucesso!")
-        navigate("/")
+        alert("Usuário cadastrado com sucesso!");
+        navigate("/");
       })
-      .catch(error => {
-        if(error.response){
-          alert(error.response.data.message)
-        }else {
-          alert("Não foi possível cadastrar")
+      .catch((error) => {
+        if (error.response) {
+          alert(error.response.data.message);
+        } else {
+          alert("Não foi possível cadastrar");
         }
-      })
-
+      });
   }
 
-  return(
+  useEffect(() => {
+    function handleKeyDown(event) {
+      if (event.key === "Enter") {
+        handleSignUp();
+      }
+    }
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [name, email, password]);
+
+  return (
     <Container>
       <Background />
 
       <Form>
-        <h1>Rocket Notes</h1>
+        <h1>RocketNotes</h1>
         <p>Aplicação para salvar e gerenciar seus links úteis.</p>
 
         <h2>Crie sua conta</h2>
 
-        <Input 
+        <Input
           placeholder="Nome"
           type="text"
           icon={FiUser}
-          onChange={e => setName(e.target.value)}
+          onChange={(e) => setName(e.target.value)}
         />
 
-        <Input 
+        <Input
           placeholder="E-mail"
           type="text"
           icon={FiMail}
-          onChange={e => setEmail(e.target.value)}
+          onChange={(e) => setEmail(e.target.value)}
         />
 
-        <Input 
+        <Input
           placeholder="Senha"
           type="password"
           icon={FiLock}
-          onChange={e => setPassword(e.target.value)}
+          onChange={(e) => setPassword(e.target.value)}
         />
 
-        <Button 
-          title="Cadastrar"
-          onClick={handleSignUp}
-        />
+        <Button title="Cadastrar" onClick={handleSignUp} />
 
-        <Link to="/">
-          Voltar para o login
-        </Link>
-
+        <Link to="/">Voltar para o login</Link>
       </Form>
-
     </Container>
-  )
+  );
 }
