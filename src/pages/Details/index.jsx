09e5bf7a0bbbfaf8081485e5c3a0b416c71ef695
emerson <emerson8096@gmail.com>
2024-06-24 09:13:15 -0300

    refactor

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
-import { Container, Links, Content } from "./styles";
 import { useParams, useNavigate } from "react-router-dom";
-
-import { api } from "../../services/api";
 
 import { Tag } from "../../components/Tag";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
 import { ButtonText } from "../../components/ButtonText";
+import { Container, Links, Content } from "./styles";
+
+import { api } from "../../services/api";
 
 export function Details() {
   const [data, setData] = useState(null);
